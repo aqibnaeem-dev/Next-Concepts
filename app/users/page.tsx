c82be2d289013e5ddb402d2c814d7412e9b825c9
aqib naeem <aqib.devsinc@gmail.com>
2023-10-10 09:47:54 -0400

    Apply basic styling

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -11,21 +11,29 @@ export default function ClientComponent() {
     const [searchPost, setSearchPost] = useState('1')
 
     return (
-        <main>
+        <div>
             <h1 className="text-4xl">Search for the specific post</h1>
 
-            <form action={submit}>
-                <input
-                    name="PostID"
-                    className="outline"
-                    type="number"
-                    value={searchPost}
-                    onChange={(event) => setSearchPost(event.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
+            <br />
+            <div className="flex justify-center align-center ">
+                <form action={submit}>
+                    <input
+                        name="PostID"
+                        className="shadow appearance-none border rounded  py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+                        type="number"
+                        value={searchPost}
+                        onChange={(event) => setSearchPost(event.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded ml-5"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
             <br />
             <br />
-        </main>
+        </div>
     )
 }
